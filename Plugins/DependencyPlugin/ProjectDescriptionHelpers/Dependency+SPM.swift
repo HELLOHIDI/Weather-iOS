8b61bb81f77e6c416968043673f5103dc1490f4b
HELLOHIDI <fbgmlwo123@naver.com>
2024-06-06 15:59:50 +0900

    [Setting] ReactorKit 세팅하기 #17

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -21,4 +21,5 @@ public extension TargetDependency.SPM {
     static let RxSwift = TargetDependency.external(name: "RxSwift")
     static let RxCocoa = TargetDependency.external(name: "RxCocoa")
     static let RxRelay = TargetDependency.external(name: "RxRelay")
+    static let ReactorKit = TargetDependency.external(name: "ReactorKit")
 }
